6e8fd3d46c58f34014fc6530c5afbee6d9d65668
Hawkpro <hawktemp@pm.me>
2024-11-08 00:59:00 +0530

    testing dynamic types

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Child from "./Child.svelte";
+  let data = {
+    value: 121,
+    name: "cde",
+    children: [
+      {
+        value: 122,
+        name: "cdeChild",
+        children: [{ value: 123, name: "abcd" }],
+      },
+    ],
+  };
+</script>
+
+Parent
+
+<Child {data} identifier="value" />
---
+++ src/routes/Child.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" generics="Identifier extends string">
+  type DataType = {
+    [K in Identifier]: number;
+  } & {
+    name: string;
+    children?: DataType[];
+  };
+
+  type Props = {
+    data: DataType;
+    identifier: string;
+  };
+
+  let { data, identifier }: Props = $props();
+  // If I could do this i can set the Dataprop type to
+  //  let { data }: DataProp<typeof identifier> = $props();
+  // This does not work as i cannot get props seperatly
+</script>
+
+from child
